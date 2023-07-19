2fc793453b189bf4da5848eaadfed521da07db2b
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-07-19 17:13:19 -0400

    updated home page and services

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,79 +2,73 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <main className='bg-white'>
-      <div className='container'>
-        <div className='mt-4'> {/* Add margin top */}
-          <div className='card bg-white text-black mb-4'>
-            <div className='card-body'>
-              <h2 className='card-title'>Services</h2>
-              <p className='card-text'>
-                At Camarco Consulting LLC, we offer a range of consulting and advisory services to businesses in various industries. Our services include:
-              </p>
-              <ul>
-                <li>Business process optimization</li>
-                <li>Strategic planning</li>
-                <li>Business analysis</li>
-                <li>Technology consulting</li>
-                <li>Process & Workflow Automation</li>
-              </ul>
-              <p>
-                We work closely with our clients to understand their unique needs and challenges, and tailor our solutions to help them achieve their goals. Whether it's improving operational efficiency, implementing new technologies, or developing effective strategies, our experienced team is here to support your business every step of the way.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className='row'>
-          <div className='col-sm-12 col-md-6 col-lg-4'>
-            <section className='card bg-white text-black mb-4' id='expertise'>
-              <div className='card-body'>
-                <h2 className='card-title'>Expertise</h2>
-                <p className='card-text'>
-                  Our expertise is enhanced by our CEO, Mickey Camarco, who has a unique perspective on the industry. Mickey Camarco has been a valued customer and employee of Nintex in the past, and their experience has given them invaluable insights into their tools and technologies. As a partner with Nintex, we leverage this deep understanding to provide our clients with exceptional solutions and results.
+      <main className="bg-white">
+        <div className="container">
+          <div className="mt-4">
+            <div className="card bg-white text-black mb-4">
+              <div className="card-body">
+                <h2 className="card-title">About</h2>
+                <p className="card-text">
+                  Camarco Consulting LLC is a trusted provider of consulting and advisory services for businesses across diverse industries. Our team of seasoned consultants specializes in optimizing operations, streamlining processes, and developing effective strategies to help clients achieve their goals. In addition to our core services in business process optimization, strategic planning, business analysis, technology consulting, and web development expertise, we offer comprehensive solutions to empower clients in enhancing their online presence and leveraging the power of digital platforms to drive growth.
                 </p>
-                <p>
-                  Mickey Camarco's firsthand experience with Nintex allows us to offer comprehensive consulting and advisory services tailored to the specific needs of businesses. We understand how to effectively leverage Nintex's offerings to optimize processes, automate workflows, and drive operational efficiency. Our expertise, combined with our partnership with Nintex, enables us to deliver innovative and impactful solutions to our clients.
-                </p>
-                <p>
-                  We stay up-to-date with the latest industry trends and best practices to ensure that our clients receive the most relevant and effective solutions. Our team's diverse skill set enables us to address complex business challenges and deliver measurable results.
+                <p className="card-text">
+                  We understand that each business is unique, and we tailor our solutions to meet specific needs. We are committed to delivering top-notch solutions that drive efficiency, productivity, and success. 
                 </p>
               </div>
-            </section>
+            </div>
           </div>
 
-          <div className='col-sm-12 col-md-6 col-lg-4'>
-            <section className='card bg-white text-black mb-4' id='approach'>
-              <div className='card-body'>
-                <h2 className='card-title'>Approach</h2>
-                <p className='card-text'>
+        {/* <div className="row">
+          <div className="col">
+            <div className="card bg-white text-black mb-4">
+              <div className="card-body">
+                <h2 className="card-title">Our Expertise</h2>
+                <p className="card-text">
+                  Our expertise is enhanced by our CEO, Mickey Camarco, who brings valuable insights from their experience as a valued customer and employee of Nintex. This firsthand experience allows us to offer comprehensive consulting and advisory services tailored to the specific needs of businesses. We understand how to effectively leverage Nintex's offerings to optimize processes, automate workflows, and drive operational efficiency.
+                </p>
+                <p className="card-text">
+                  By staying up-to-date with the latest industry trends and best practices, we ensure that our clients receive the most relevant and effective solutions. Our team's diverse skill set enables us to address complex business challenges and deliver measurable results.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div> */}
+
+        <div className="row">
+          <div className="col">
+            <div className="card bg-white text-black mb-4">
+              <div className="card-body">
+                <h2 className="card-title">Approach</h2>
+                <p className="card-text">
                   At Camarco Consulting LLC, we follow a comprehensive approach to provide value-added solutions to our clients. Our approach includes:
                 </p>
                 <ul>
-                  <li>Thorough assessment: We conduct a thorough analysis of your business processes, operations, and challenges to identify areas for improvement.</li>
+                  <li>Thorough assessment: We conduct a detailed analysis of your business processes, operations, and challenges to identify areas for improvement.</li>
                   <li>Collaborative planning: We work closely with your team to develop customized strategies and action plans that align with your business objectives.</li>
                   <li>Implementation and support: We assist in implementing the recommended solutions and provide ongoing support to ensure successful execution and long-term sustainability.</li>
                   <li>Continuous improvement: We continuously monitor and evaluate the effectiveness of our solutions, making adjustments as needed to drive continuous improvement.</li>
                 </ul>
-                <p>
+                <p className="card-text">
                   Our client-centric approach, combined with our expertise and commitment to excellence, sets us apart as a trusted consulting partner for businesses looking to achieve sustainable growth and success.
                 </p>
               </div>
-            </section>
+            </div>
           </div>
+        </div>
 
-          <div className='col-sm-12 col-md-6 col-lg-4'>
-            <section className='card bg-white text-black mb-4' id='partnership'>
-              <div className='card-body'>
-                <h2 className='card-title'>Partnership with Nintex</h2>
-                <p className='card-text'>
-                  As part of our commitment to providing cutting-edge solutions, we have partnered with Nintex, a leading provider of process automation and management solutions. This partnership allows us to offer our clients access to Nintex's advanced tools and technologies, which can significantly enhance operational efficiency, productivity, and growth in their business.
+        <div className="row">
+          <div className="col">
+            <div className="card bg-white text-black mb-4">
+              <div className="card-body">
+                <h2 className="card-title">Partnerships</h2>
+                <p className="card-text">
+                  As part of our commitment to providing cutting-edge solutions, we have partnered with Nintex, a leading provider of process automation and management solutions. This partnership allows us to offer our clients access to Nintex's advanced tools and technologies, which can significantly enhance operational efficiency, productivity, and business growth.
                 </p>
-                <p>
-                  Nintex offers a range of solutions, including process automation, workflow management, document generation, and more. Through our partnership, we can leverage Nintex's expertise to streamline and optimize your business processes, driving cost savings and improving overall performance.
+                <p className="card-text">
+                  Nintex offers a range of solutions, including process automation, workflow management, document generation, and more. Through our partnership, we leverage Nintex's expertise to streamline and optimize your business processes, driving cost savings and improving overall performance.
                 </p>
               </div>
-            </section>
+            </div>
           </div>
         </div>
       </div>
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -16,15 +16,10 @@ const ModalComponent = ({ show, onHide, title, content }) => {
 };
 
 const Services = () => {
-  const [showModal1, setShowModal1] = useState(false);
   const [showModal2, setShowModal2] = useState(false);
   const [showModal3, setShowModal3] = useState(false);
   const [showModal4, setShowModal4] = useState(false);
   const [showModal5, setShowModal5] = useState(false);
-
-  const toggleModal1 = () => {
-    setShowModal1(!showModal1);
-  };
 
   const toggleModal2 = () => {
     setShowModal2(!showModal2);
@@ -44,99 +39,88 @@ const Services = () => {
 
   return (
     <Container>
-      <Row>
-        <Col xs={12} sm={6} md={4} lg={3}>
-          <Card className='rounded bg-white text-black text-center'>
-            <Card.Body>
-              <Card.Title>Consultation</Card.Title>
-              <Card.Text>
-                We offer free consultation to understand your needs and explore how our services can assist your business.
-              </Card.Text>
-              <Button variant="primary" onClick={toggleModal1}>Find out more</Button>
-            </Card.Body>
-          </Card>
-          <ModalComponent
-            show={showModal1}
-            onHide={toggleModal1}
-            title="Consultation"
-            content="We offer free consultation to understand your needs and explore how our services can assist your business. Our team of experienced consultants will take the time to listen to your goals and challenges, providing personalized advice and recommendations. Whether you're looking to improve efficiency, implement new technologies, or enhance your overall business strategy, we're here to help. During the consultation, we'll discuss potential solutions and determine if we're a good fit for your requirements. We believe in building strong partnerships with our clients and ensuring that our services align with their objectives."
-          />
-        </Col>
+      <section className='text-center'>
+        <br></br>
+        <h2>Our Services</h2>
+        <p>
+          We are committed to empowering businesses and driving success. Our team of experienced consultants understands the importance of personalized advice and recommendations. Whether you seek to improve efficiency, implement new technologies, or enhance your overall business strategy, we're here to provide guidance tailored to your objectives. Through collaborative discussions, we'll identify potential solutions and determine if we're the right fit for your requirements. We prioritize building strong partnerships with our clients, ensuring that our services align with their objectives for long-term success.
+        </p>
+        <br></br>
+        <Row>
+          <Col xs={12} sm={6} md={4} lg={3}>
+            <Card className='rounded bg-white text-black'>
+              <Card.Body>
+                <Card.Title>Process Transformation</Card.Title>
+                <Card.Text>
+                  Optimize and streamline your company's workflows and operations.
+                </Card.Text>
+                <Button variant="primary" onClick={toggleModal2}>Find out more</Button>
+              </Card.Body>
+            </Card>
+            <ModalComponent
+              show={showModal2}
+              onHide={toggleModal2}
+              title="Business Process Transformation"
+              content="Our business process transformation service aims to optimize and streamline your company's workflows and operations. We work closely with your team to analyze existing processes, identify bottlenecks, and implement efficient solutions. Through process mapping, automation, and workflow redesign, we help you achieve greater productivity and cost-effectiveness."
+            />
+          </Col>
 
-        <Col xs={12} sm={6} md={4} lg={3}>
-          <Card className='rounded bg-white text-black text-center'>
-            <Card.Body>
-              <Card.Title>Business Process Transformation</Card.Title>
-              <Card.Text>
-                Our business process transformation service aims to optimize and streamline your company's workflows and operations.
-              </Card.Text>
-              <Button variant="primary" onClick={toggleModal2}>Find out more</Button>
-            </Card.Body>
-          </Card>
-          <ModalComponent
-            show={showModal2}
-            onHide={toggleModal2}
-            title="Business Process Transformation"
-            content="Our business process transformation service aims to optimize and streamline your company's workflows and operations. We work closely with your team to analyze existing processes, identify bottlenecks, and implement efficient solutions. Through process mapping, automation, and workflow redesign, we help you achieve greater productivity and cost-effectiveness."
-          />
-        </Col>
+          <Col xs={12} sm={6} md={4} lg={3}>
+            <Card className='rounded bg-white text-black'>
+              <Card.Body>
+                <Card.Title>Web Development</Card.Title>
+                <Card.Text>
+                  Full-stack web development services for seamless functionality and an exceptional user experience.
+                </Card.Text>
+                <Button variant="primary" onClick={toggleModal3}>Find out more</Button>
+              </Card.Body>
+            </Card>
+            <ModalComponent
+              show={showModal3}
+              onHide={toggleModal3}
+              title="Full Stack Web Development"
+              content="Our full-stack web development services encompass front-end and back-end development, ensuring seamless functionality and an exceptional user experience. We utilize modern technologies and frameworks to create responsive, interactive, and visually appealing websites and web applications. Whether you need a simple website, a complex web application, or an e-commerce platform, our full-stack web development services can bring your vision to life. We prioritize clean code, scalability, security, and performance to deliver solutions that meet your business requirements and exceed your expectations."
+            />
+          </Col>
 
-        <Col xs={12} sm={6} md={4} lg={3}>
-          <Card className='rounded bg-white text-black text-center'>
-            <Card.Body>
-              <Card.Title>Web Development</Card.Title>
-              <Card.Text>
-                Our full-stack web development services encompass front-end and back-end development, ensuring seamless functionality and an exceptional user experience.
-              </Card.Text>
-              <Button variant="primary" onClick={toggleModal3}>Find out more</Button>
-            </Card.Body>
-          </Card>
-          <ModalComponent
-            show={showModal3}
-            onHide={toggleModal3}
-            title="Full Stack Web Development"
-            content="Our full-stack web development services encompass front-end and back-end development, ensuring seamless functionality and an exceptional user experience. We utilize modern technologies and frameworks to create responsive, interactive, and visually appealing websites and web applications. Whether you need a simple website, a complex web application, or an e-commerce platform, our full-stack web development services can bring your vision to life. We prioritize clean code, scalability, security, and performance to deliver solutions that meet your business requirements and exceed your expectations."
-          />
-        </Col>
-      </Row>
+          <Col xs={12} sm={6} md={4} lg={3}>
+            <Card className='rounded bg-white text-black'>
+              <Card.Body>
+                <Card.Title>Nintex Training</Card.Title>
+                <Card.Text>
+                  Comprehensive training programs to help your team leverage the Nintex platform's full potential.
+                </Card.Text>
+                <Button variant="primary" onClick={toggleModal4}>Find out more</Button>
+              </Card.Body>
+            </Card>
+            <ModalComponent
+              show={showModal4}
+              onHide={toggleModal4}
+              title="Nintex Training"
+              content="Our Nintex training service provides comprehensive training programs to help your team harness the full potential of the Nintex platform. We offer tailored training sessions that cover various aspects of Nintex, including workflow automation, forms design, and process optimization. Our experienced trainers will guide your team through hands-on exercises and real-world scenarios to ensure practical understanding and skill development. Whether you're new to Nintex or looking to enhance your existing expertise, our training programs can empower your team to leverage Nintex effectively."
+            />
+          </Col>
 
-      <Row>
-        <Col xs={12} sm={6} md={4} lg={3}>
-          <Card className='rounded bg-white text-black text-center'>
-            <Card.Body>
-              <Card.Title>Nintex Training</Card.Title>
-              <Card.Text>
-                Our Nintex training service provides comprehensive training programs to help your team harness the full potential of the Nintex platform.
-              </Card.Text>
-              <Button variant="primary" onClick={toggleModal4}>Find out more</Button>
-            </Card.Body>
-          </Card>
-          <ModalComponent
-            show={showModal4}
-            onHide={toggleModal4}
-            title="Nintex Training"
-            content="Our Nintex training service provides comprehensive training programs to help your team harness the full potential of the Nintex platform. We offer tailored training sessions that cover various aspects of Nintex, including workflow automation, forms design, and process optimization. Our experienced trainers will guide your team through hands-on exercises and real-world scenarios to ensure practical understanding and skill development. Whether you're new to Nintex or looking to enhance your existing expertise, our training programs can empower your team to leverage Nintex effectively."
-          />
-        </Col>
-
-        <Col xs={12} sm={6} md={4} lg={3}>
-          <Card className='rounded bg-white text-black text-center'>
-            <Card.Body>
-              <Card.Title>Nintex Support</Card.Title>
-              <Card.Text>
-                Our Nintex support service is designed to provide ongoing assistance and troubleshooting for your Nintex implementations.
-              </Card.Text>
-              <Button variant="primary" onClick={toggleModal5}>Find out more</Button>
-            </Card.Body>
-          </Card>
-          <ModalComponent
-            show={showModal5}
-            onHide={toggleModal5}
-            title="Nintex Support"
-            content="Our Nintex support service is designed to provide ongoing assistance and troubleshooting for your Nintex implementations. We understand that technology issues can arise, and our dedicated support team is here to help you resolve them efficiently. When you encounter any challenges or problems with your Nintex workflows, forms, or other related issues, our experts will work closely with you to identify and resolve them. Our support process involves thoroughly analyzing the reported issue to understand its root cause, providing step-by-step guidance and best practices to help you address the problem, utilizing our expertise and experience to troubleshoot and resolve the issue directly, and ensuring clear and timely communication throughout the resolution process. In cases where the issue requires further assistance, we have direct channels to connect with Nintex support to escalate and resolve complex problems. Our goal is to ensure the smooth functioning of your Nintex processes and help you overcome any obstacles along the way."
-          />
-        </Col>
-      </Row>
+          <Col xs={12} sm={6} md={4} lg={3}>
+            <Card className='rounded bg-white text-black'>
+              <Card.Body>
+                <Card.Title>Nintex Support</Card.Title>
+                <Card.Text>
+                  Ongoing assistance and troubleshooting for your Nintex implementations.
+                </Card.Text>
+                <Button variant="primary" onClick={toggleModal5}>Find out more</Button>
+              </Card.Body>
+            </Card>
+            <ModalComponent
+              show={showModal5}
+              onHide={toggleModal5}
+              title="Nintex Support"
+              content="Our Nintex support service is designed to provide ongoing assistance and troubleshooting for your Nintex implementations. We understand that technology issues can arise, and our dedicated support team is here to help you resolve them efficiently. When you encounter any challenges or problems with your Nintex workflows, forms, or other related issues, our experts will work closely with you to identify and resolve them. Our support process involves thoroughly analyzing the reported issue to understand its root cause, providing step-by-step guidance and best practices to help you address the problem, utilizing our expertise and experience to troubleshoot and resolve the issue directly, and ensuring clear and timely communication throughout the resolution process. In cases where the issue requires further assistance, we have direct channels to connect with Nintex support to escalate and resolve complex problems. Our goal is to ensure the smooth functioning of your Nintex processes and help you overcome any obstacles along the way."
+            />
+          </Col>
+        </Row>
+        <br></br>
+      </section>
     </Container>
   );
 };
